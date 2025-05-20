d22bc6800ffcb97eebccb0f55ccd1db844bfeb37
shanto0241 <shantonu2050@gmail.com>
2025-05-21 02:13:16 +0600

    check again

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,28 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-import { protectRoute } from "./middleware/protectRoute.js";
-
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
-import searchRoutes from "./routes/search.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
-const PORT = ENV_VARS.PORT || 5000;
+
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
-const __dirname = path.resolve();
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
-// Serve static files from the React app
 if (ENV_VARS.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
@@ -33,6 +33,6 @@ if (ENV_VARS.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-  // console.log("Server started at http://localhost:" + PORT);
+  console.log("Server started at http://localhost:" + PORT);
   connectDB();
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: "public", // Default output directory
+    outDir: "dist", // Default output directory
   },
 });
